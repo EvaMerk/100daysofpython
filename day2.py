0f2adc5ab0a9ab0c28e025fbea8b463d677f5fed
EvaMerk <92169575+EvaMerk@users.noreply.github.com>
2022-04-03 14:31:11 +0200

    exercise 3 - months, weeks, days til age of 90

--- day2.py
+++ day2.py
@@ -24,3 +24,19 @@ height = float(height)
 weight = float(weight)
 bmi = round(weight / (height * height), 2)
 print(bmi)
+
+# Exercise 3:
+# Create a program using maths and f-Strings that tells us how many days, weeks, months we have left if we live until 90 years old.
+# It will take your current age as the input and output a message with our time left in this format: You have x days, y weeks, and z months left.
+# Where x, y and z are replaced with the actual calculated numbers.
+# Warning your output should match the Example Output format exactly, even the positions of the commas and full stops.
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+max_age = 90
+num_months = round((max_age - int(age)) * 12)
+num_weeks = round((max_age - int(age)) * 52)
+num_days = round((max_age - int(age)) * 365)
+print(f"You have {num_days} days, {num_weeks} weeks and {num_months} months left.")
