358a0df1f99a463d413cdd2c28b719efaf66bdab
EvaMerk <92169575+EvaMerk@users.noreply.github.com>
2022-04-03 14:10:03 +0200

    challenge 2 - bmi calculator

--- day2.py
+++ day2.py
@@ -1,5 +1,6 @@
-#Write a program that adds the digits in a 2 digit number. e.g. if the input was 35, then the output should be 3 + 5 = 8
-#Warning. Do not change the code on lines 1-3. Your program should work for different inputs. e.g. any two-digit number.
+# Exercise 1:
+# Write a program that adds the digits in a 2 digit number. e.g. if the input was 35, then the output should be 3 + 5 = 8
+# Warning. Do not change the code on lines 1-3. Your program should work for different inputs. e.g. any two-digit number.
 
 # 🚨 Don't change the code below 👇
 two_digit_number = input("Type a two digit number: ")
@@ -9,3 +10,17 @@ two_digit_number = input("Type a two digit number: ")
 digit_1 = int(two_digit_number[0])
 digit_2 = int(two_digit_number[1])
 print(digit_1 + digit_2)
+
+# Exercise 2:
+# Write a program that calculates the Body Mass Index (BMI) from a user's weight and height.
+# The BMI is a measure of some's weight taking into account their height. e.g. If a tall person and a short person both weigh the same amount, the short person is usually more overweight.
+# The BMI is calculated by dividing a person's weight (in kg) by the square of their height (in m):
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+#Write your code below this line 👇
+height = float(height)
+weight = float(weight)
+bmi = int(weight / (height * height))
+print(bmi)
