8a0df49642cf743f08503b255164be259d90293c
EvaMerk <92169575+EvaMerk@users.noreply.github.com>
2022-04-25 15:06:34 +0200

    ex1 - heads or tails

---
+++ day4.py
@@ -0,0 +1,18 @@
+#Ex 1 - Heads or Tails
+#You are going to write a virtual coin toss program. It will randomly tell the user "Heads" or "Tails".
+#Important, the first letter should be capitalised and spelt exactly like in the example e.g. Heads, not heads.
+#When you run the code, just use a random number as the seed. e.g. 67346 It doesn't matter what you chose, it's only for our testing code to check your work.
+#Remember to use the random module
+#Hint: Remember to import the random module here at the top of the file. 🎲
+import random
+# 🚨 Don't change the code below 👇
+test_seed = int(input("Create a seed number: "))
+random.seed(test_seed)
+ # 🚨 Don't change the code above 👆 It's only for testing your code.
+	 
+#Write the rest of your code below this line 👇
+random_side = random.randint(0, 1)
+if random_side == 1:
+    print("Heads")
+else:
+    print("Tails")
