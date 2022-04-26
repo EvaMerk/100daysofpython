39d88dbf1ab5dec8c75536470e10c6f28a2cbd65
EvaMerk <92169575+EvaMerk@users.noreply.github.com>
2022-04-26 09:12:51 +0200

    ex 2 - highest score

--- day5.py
+++ day5.py
@@ -25,3 +25,25 @@ for height in student_heights:
     average_height = round(total_height / counter, 0)
     
 print(average_height)
+
+#ex 2 - highest score
+#You are going to write a program that calculates the highest score from a List of scores.
+#e.g. student_scores = [78, 65, 89, 86, 55, 91, 64, 89]
+#Important you are not allowed to use the max or min functions. The output words must match the example.
+# 🚨 Don't change the code below 👇
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+highest_score = 0
+
+for score in student_scores:
+    if score > highest_score:
+        highest_score = score
+    else:
+        highest_score= highest_score
+        
+print(f"The highest score in the class is: {highest_score}")
