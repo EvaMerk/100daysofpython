3e77cf93a1e14d1908966eedb5233c41427b6404
EvaMerk <92169575+EvaMerk@users.noreply.github.com>
2022-04-27 19:46:13 +0200

    final game

--- day7.py
+++ day7.py
@@ -61,17 +61,17 @@ stages = ['''
 ''']
 
 end_of_game = False
-word_list = ["ardvark", "baboon", "camel"]
-chosen_word = random.choice(word_list)
+
+import day7_hangman_words
+chosen_word = random.choice(day7_hangman_words.word_list)
 word_length = len(chosen_word)
 
-#TODO-1: - Create a variable called 'lives' to keep track of the number of lives left. 
-#Set 'lives' to equal 6.
 lives = 6
 
 #Testing code
 #print(f'Pssst, the solution is {chosen_word}.')
-
+from day7_art import logo, stages
+print(logo)
 #Create blanks
 display = []
 for _ in range(word_length):
@@ -80,26 +80,24 @@ for _ in range(word_length):
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
+    if guess in display:
+      print(f"You've already guessed {guess}")
     #Check guessed letter
     for position in range(word_length):
         letter = chosen_word[position]
         # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
         if letter == guess:
             display[position] = letter
-    
+
     if guess not in chosen_word:
       lives -=1
-      print("No match, sorry")
+      print("You've guessed {guess}. That's not in the word. You lose a life, sorry.")
       
       if lives == 0:
         end_of_game = True
         print("You lose.")
     
     print(stages[lives])
-
-    #TODO-2: - If guess is not a letter in the chosen_word,
-    #Then reduce 'lives' by 1. 
-    #If lives goes down to 0 then the game should stop and it should print "You lose."
 
     #Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
@@ -108,5 +106,3 @@ while not end_of_game:
     if "_" not in display:
         end_of_game = True
         print("You win.")
-
-    #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
