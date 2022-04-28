65c3452d2450f861ad525ebbcb6418e3db3ad94b
EvaMerk <92169575+EvaMerk@users.noreply.github.com>
2022-04-28 11:09:38 +0200

    Ex 2 - Prime number checker

--- day8.py
+++ day8.py
@@ -22,3 +22,19 @@ test_w = int(input("Width of wall: "))
 coverage = 5
 paint_calc(height=test_h, width=test_w, cover=coverage)
 
+#Ex 2 - Prime number checker
+#Write your code below this line 👇
+def prime_checker(number):
+    is_prime = True
+    for i in range(2, number):
+        if number % i == 0:
+            is_prime = False
+    if is_prime == True:
+        print("It's a prime number.")
+    else:
+        print("It's not a prime number.")
+#Write your code above this line 👆
+    
+#Do NOT change any of the code below👇
+n = int(input("Check this number: "))
+prime_checker(number=n)
