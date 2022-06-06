0a288246df0f026a1c6b9671e7a7a6ae8a51ddf7
EvaMerk <92169575+EvaMerk@users.noreply.github.com>
2022-06-06 14:48:54 +0200

    Calculator finished

--- day10.py
+++ day10.py
@@ -34,43 +34,48 @@ print(days)
 #Project: Calculator
 #Step 1
 #Calculator
-
-#Add
 def add(n1, n2):
-	return n1 + n2
+  return n1 + n2
 
-#Subtract
 def subtract(n1, n2):
-	return n1 - n2
+  return n1 - n2
 
-#Multiply
 def multiply(n1, n2):
-	return n1 * n2
+  return n1 * n2
 
-#Divide
 def divide(n1, n2):
-	return n1 / n2
+  return n1 / n2
 
-math_operation = {
-	"+": add,
-	"-": subtract,
-	"*": multiply,
-	"/": divide
+operations = {
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide
 }
 
-num1 = int(input("What's the first number?:\n"))
-for symbol in math_operation:
-	print(symbol)
-operation = input("Which math operation do you want to perform? Pick a symbol from the lines above:\n")
-num2 = int(input("What's the second number?:\n"))
+def calculator():
+  print(logo)
 
-for math_function in math_operation:
-	math_function = operation
-	function = math_operation[math_function]
-	answer = function(num1, num2)
-	
-print(f"{num1} {operation} {num2} = {answer}")
+  num1 = float(input("What's the first number?: "))
+  for symbol in operations:
+    print(symbol)
+  should_continue = True
+ 
+  while should_continue:
+    operation_symbol = input("Pick an operation: ")
+    num2 = float(input("What's the next number?: "))
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(num1, num2)
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
 
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y':
+      num1 = answer
+    else:
+      should_continue = False
+      clear()
+      calculator()
+
+calculator()
 
 
 
